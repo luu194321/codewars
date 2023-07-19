62c2698a567d4dce6ab4cd94fbc00dc35067e670
Alex Luu <alex__luu@hotmail.com>
2023-07-19 18:10:33 -0500

    kyu7+8 7/19/23

---
+++ kyu7/index.js
@@ -0,0 +1,16 @@
+"use strict";
+/*
+// Highest and Lowest
+// In this little assignment you are given a string of space separated numbers, and have to return the highest and lowest number.
+
+function highAndLow(numbers) {
+  let answer = [];
+  const array = numbers.split(" ").map(Number);
+
+  array.sort((a, b) => a - b);
+  answer.push(array[array.length - 1]);
+  answer.push(array[0]);
+
+  return answer.join(" ");
+}
+*/
---
+++ kyu8/index.js
@@ -0,0 +1,12 @@
+'use strict';
+
+
+/* 
+// Even or Odd 
+
+// Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+
+const evenOrOdd=(number)=> number%2===0? 'Even' : 'Odd'
+
+*/
+
