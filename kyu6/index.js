3109b177be35764f81766bfeba8098a0038a5091
Alex Luu <alex__luu@hotmail.com>
2023-07-24 21:30:05 -0500

    7/24/23 coding warm up with two 6kyus

--- kyu6/index.js
+++ kyu6/index.js
@@ -32,6 +32,8 @@ function findOdd(A) {
 
 */
 
+//------------------------------------------------------------------//
+
 
 /*
 // Array.diff 20 min
@@ -45,3 +47,54 @@ function arrayDiff(a, b) {
 
 
 */
+
+//------------------------------------------------------------------//
+
+/*
+//
+// Stop gninnipS My sdroW!
+// Write a function that takes in a string of one or more words, and returns the same string, but with all five or more letter words reversed (Just like the name of this Kata). Strings passed in will consist of only letters and spaces. Spaces will be included only when more than one word is present.
+
+// Examples:
+
+// spinWords( "Hey fellow warriors" ) => returns "Hey wollef sroirraw" 
+// spinWords( "This is a test") => returns "This is a test" 
+// spinWords( "This is another test" )=> returns "This is rehtona test"
+
+function spinWords(string){
+//commented out code is my thought process inside the fx
+//   const arrayString = string.split(' ')
+  
+//   const answer = arrayString.map((el)=> el.length<5? el : el.split('').reverse().join('') )
+
+  return  string.split(' ').map(el=>el.length<5? el: el.split('').reverse().join('')).join(' ')
+//   return answer.join(' ')
+}
+*/
+
+
+//------------------------------------------------------------------//
+/*
+// our order, please
+// Your task is to sort a given string. Each word in the string will contain a single number. This number is the position the word should have in the result.
+
+// Note: Numbers can be from 1 to 9. So 1 will be the first word (not 0).
+
+// If the input string is empty, return an empty string. The words in the input String will only contain valid consecutive numbers.
+
+function order(words){
+  // this was initially coded to try to create a separate array of just integers and use that to sort
+  //   const numberPositionArray = words.split(' ').map(el=>{
+  //     const initialIndex = el.match(/\d+/);
+  //     console.log('var:',initialIndex)
+  //     return initialIndex ? Number(initialIndex[0]) : null
+      
+  //   });
+
+    const answer = words.split(' ').sort((a,b)=>{
+      return a.match(/\d+/) - b.match(/\d+/)
+    })
+
+    return answer.join(' ')
+  }
+  */
