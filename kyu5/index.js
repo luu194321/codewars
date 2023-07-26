842fc2351691d3770bd0bf16101ccfad1964c629
Alex Luu <alex__luu@hotmail.com>
2023-07-26 16:31:08 -0500

    7/26/23 2x5kyu

--- kyu5/index.js
+++ kyu5/index.js
@@ -1,6 +1,8 @@
 "use strict";
 
+/*
 // Convert PascalCase string into snake_case
+// Complete the function/method so that it takes a PascalCase string and returns the string in snake_case notation. Lowercase characters can be numbers. If the method gets a number as input, it should return a string.
 
 function toUnderscore(string) {
   if (typeof string === "number") {
@@ -25,8 +27,52 @@ function toUnderscore(string) {
       answer += string[i];
     }
   }
-
   console.log(answer);
 
   return answer;
+}
+*/
+
+/*
+// RGB To Hex Conversion
+
+// The rgb function is incomplete. Complete it so that passing in RGB decimal values will result in a hexadecimal representation being returned. Valid decimal values for RGB are 0 - 255. Any values that fall out of that range must be rounded to the closest valid value.
+// Note: Your answer should always be 6 characters long, the shorthand with 3 will not work here.
+// The following are examples of expected output values:
+
+function rgb(r, g, b) {
+  const R = Math.max(0, Math.min(255, Math.round(r)));
+  const G = Math.max(0, Math.min(255, Math.round(g)));
+  const B = Math.max(0, Math.min(255, Math.round(b)));
+
+  const hexR = R.toString(16).padStart(2, "0");
+  const hexG = G.toString(16).padStart(2, "0");
+  const hexB = B.toString(16).padStart(2, "0");
+
+  const answer = hexR.toUpperCase() + hexG.toUpperCase() + hexB.toUpperCase();
+
+  return answer;
+}
+*/
+// Human Readable Time
+function humanReadable(seconds) {
+  if (seconds < 0 || seconds > 359999) {
+    return null;
+  }
+
+  if (seconds === 0) {
+    return "00:00:00";
+  }
+
+  const hours = Math.floor(seconds / 3600);
+  const minutes = Math.floor((seconds % 3600) / 60);
+  const remainingSeconds = seconds % 60;
+
+  const formattedHours = String(hours).padStart(2, "0");
+  const formattedMinutes = String(minutes).padStart(2, "0");
+  const formattedSeconds = String(remainingSeconds).padStart(2, "0");
+
+  const formattedTime = `${formattedHours}:${formattedMinutes}:${formattedSeconds}`;
+
+  return formattedTime;
 }
