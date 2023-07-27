f69365aef998f472dd47cf922be8d31c5793e046
Alex Luu <alex__luu@hotmail.com>
2023-07-27 17:44:35 -0500

    began working on kyu6 - left unfinished

--- kyu5/index.js
+++ kyu5/index.js
@@ -54,7 +54,18 @@ function rgb(r, g, b) {
   return answer;
 }
 */
+
+
+/*
 // Human Readable Time
+// Write a function, which takes a non-negative integer (seconds) as input and returns the time in a human-readable format (HH:MM:SS)
+
+// HH = hours, padded to 2 digits, range: 00 - 99
+// MM = minutes, padded to 2 digits, range: 00 - 59
+// SS = seconds, padded to 2 digits, range: 00 - 59
+// The maximum time never exceeds 359999 (99:59:59)
+
+// You can find some examples in the test fixtures.
 function humanReadable(seconds) {
   if (seconds < 0 || seconds > 359999) {
     return null;
@@ -76,3 +87,4 @@ function humanReadable(seconds) {
 
   return formattedTime;
 }
+*/
--- kyu7/index.js
+++ kyu7/index.js
@@ -14,3 +14,85 @@ function highAndLow(numbers) {
   return answer.join(" ");
 }
 */
+
+/*
+// Vowel Count
+// Return the number (count) of vowels in the given string.We will consider a, e, i, o, u as vowels for this Kata (but not y). The input string will only consist of lower case letters and/or spaces.
+function getCount(str) {
+  const vowels = ["a", "e", "i", "o", "u"];
+
+  const answer = str.split("").reduce((acc, cur, i, arr) => {
+    if (vowels.includes(cur)) acc += 1;
+
+    return acc;
+  }, 0);
+
+  return answer;
+}
+*/
+
+// Dbftbs Djqifs
+
+function encryptor (key, message) {
+  let newIndex;
+  let answer='';
+  // check for negative key
+  let alphabetArray = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 
+                         'h', 'i', 'j', 'k', 'l', 'm', 'n', 
+                         'o', 'p', 'q', 'r', 's', 't', 'u', 
+                         'v', 'w', 'x', 'y', 'z'];
+  
+  const lowercaseRegExp = /[a-z]/
+  const uppercaseRegExp = /[A-Z]/
+  
+  // const exceedingIndex = arr[i] === undefined?
+
+  //capital/lowercase
+  // index exceeds length: pos/neg(2)
+  // key: pos/neg(2) ---> exceeding index
+    
+  const msg = message.forEach((el,i,arr)=>{
+    if(el === ' ') return answer+= el;
+
+    //-----check for capital----
+    if(el.match(uppercaseRegExp)!== null && key>alphabetArray.length){
+      //---capital and exceeding index
+      if (arr[el+key] === undefined)
+      
+      //capital and key>alphabet.length
+      if (key>alphabetArray.length){
+        newIndex= key%alphabetArray.length
+        return answer+= alphabetArray[i+newIndex].toUpperCase();
+      } else if (key>0 && key<alphabetArray.length){
+        return answer+= alphabetArray[i+]
+      }
+      //capital and negative
+      
+      if (key<0 && key>-25){
+        
+        alphabetArray = alphabetArray.reverse()
+        return answer+= alphabetArray[]
+      }
+      
+      return answer+= alphabetArray[]
+  
+      
+      
+    }
+    
+    // check for key higher than alphabet
+    if (key>alphabetArray.length)newIndex= key%alphabetArray.length
+    
+    if(key<0)
+    // check for lower case
+  
+    if (el.match(lowercaseRegExp)!== null){
+      
+      
+    }
+    
+  })
+    
+  
+    return '';
+  }
